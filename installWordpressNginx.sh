8a3ba8e6042f7001a01260e9398126fcc0b76726
MatheusLeao <mathegiov@hotmail.com>
2020-01-06 14:33:21 -0300

    procedimento bash Nginx + Wordpress

---
+++ installWordpressNginx.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+yum install nginx php php-mysql -y
+cd /var/www/html
+wget https://wordpress.org/wordpress-5.1.1.tar.gz
+tar -xzf wordpress-5.1.1.tar.gz
+cp -r wordpress/* /var/www/html/
+rm -rf wordpress
+rm -rf wordpress-5.1.1.tar.gz
+chmod -R 755 wp-content
+chown -R nginx:nginx wp-content
+systemctl start nginx
+systemctl enable nginx
